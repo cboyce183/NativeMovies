a1f9b2a41324e82a58b998989d3ace0047bbcb79
Charles Boyce <wxaz020@live.rhul.ac.uk>
2018-11-25 15:50:26 +0000

    feat: viewer view + ISSUE: navigation not playing nice with redux

---
+++ NativeMovies/components/viewer/viewer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Viewer extends Component {
+  render() {
+    return (
+      <Text>
+        placeholder
+      </Text>
+    );
+  }
+}
--- NativeMovies/index.js
+++ NativeMovies/index.js
@@ -2,8 +2,9 @@ import { AppRegistry } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import App from './App';
+import Viewer from './components/viewer/viewer';
 
-const Root = createStackNavigator({App});
+const Root = createStackNavigator({App, Viewer});
 const Start = createAppContainer(Root);
 
 AppRegistry.registerComponent("NativeMovies", () => Start);

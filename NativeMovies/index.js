9b24ac0e2869d3ac5f434f244ca819fd4907bdb7
Charles Boyce <wxaz020@live.rhul.ac.uk>
2018-11-25 15:09:22 +0000

    feat: navigation and empty app shell

--- NativeMovies/index.js
+++ NativeMovies/index.js
@@ -1,7 +1,9 @@
-/** @format */
+import { AppRegistry } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = createStackNavigator({App});
+const Start = createAppContainer(Root);
+
+AppRegistry.registerComponent("NativeMovies", () => Start);
